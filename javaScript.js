15349b6d2951fd9d27c84d6506af0ef95c032942
Kenneth Holt <kingholt@gmail.com>
2022-11-17 19:12:37 -0800

    add notes

--- javaScript.js
+++ javaScript.js
@@ -1,80 +1,127 @@
 
-/* 
-function rateMyPage(){
-    let rating = prompt("how many stars would you rate my page?");
-    for (let i = 0; i < rating; i++){
-        document.write(",img src='star.jpg' alt="shiny gold star" />
-    }
-} */
-
-function first(){
-alert('Halt!, who goes there??');
-console.log('this is my first attempt at this!');
-let usersName = prompt("Who is it that wishes to enter?");
-console.log("The user's name is: ", usersName);
-document.write("Hello and welcome to my first attempt at this, " + usersName);
-}
-first();
+// /* 
+// function rateMyPage(){
+//     let rating = prompt("how many stars would you rate my page?");
+//     for (let i = 0; i < rating; i++){
+//         document.write(",img src='star.jpg' alt="shiny gold star" />
+//     }
+// } */
 
 
-function userName() {
-    let user=prompt ("what is your name??");
-
-    if (user === ""){
-        user=prompt ("but seriously give me");
-    }
-    else {
-        console.log(user);
-        document.write("let's begin adv" + user);
-    }
-
-    return user;
-
-}
-
-function exp() {
-    document.write("get ready for fun");
-
-if (response == "yes"){
-    alert("youve come here");
-}
-
-}
 
 
-function userName(){
-    let user=prompt ("what is name");
-
-    while (user=== "") {
-        user=prompt ("but serisolky");
-    }
-
-    console.log(user);
-    document.write("lets begin adcventure" + user);
-
-    return user;
-}
 
 
-function myCount() {
-    let count = prompt("pick a number 1-5");
-    for (i = 0; i < count; i++) {
-        console.log(i);
-    }
-}
 
-myCount();
+// <script>
+//     alert('Halt!, who goes there??');
+//     console.log('this is my first attempt at this!');
+//     let usersName = prompt("Who is it that wishes to enter?");
+//     console.log("The user's name is: ", usersName);
+//     document.write("Hello and welcome to my first attempt at this, " + usersName);
+    
+//     let response = prompt("how can we help you?")
+
+// if (response == "food") {
+//     alert("you are in for a treat");
+//     document.write("you are in for a treat");
+//     else if (response == "drink") {
+//     alert("we have some good desserts");
+//     document.write("we have some good desserts");
+//     }
+// }
+// </script>
+// first();
 
 
-function selectaleaf() {
-    let selection = prompt ('how many apps would you like?');
-    while (selection < 1 || selection >5 ) {
-        selection = prompt ('must be between 1 and 5')
-}
-for (let i = 0; i < selection; i++) {
-    console.log(i)
-    document.write("<img src= 'images/stuff alt='leaf' />")
-}
+// function userName() {
+//     let user=prompt ("what is your name??");
+
+//     if (user === ""){
+//         user=prompt ("but seriously give me");
+//     }
+//     else {
+//         console.log(user);
+//         document.write("let's begin adv" + user);
+//     }
+
+//     return user;
+
+// }
+
+// function exp() {
+//     document.write("get ready for fun");
+
+// if (response == "yes"){
+//     alert("youve come here");
+// }
+
+// }
+
+
+// function userName(){
+//     let user=prompt ("what is name");
+
+//     while (user=== "") {
+//         user=prompt ("but serisolky");
+//     }
+
+//     console.log(user);
+//     document.write("lets begin adcventure" + user);
+
+//     return user;
+// }
+
+
+// function myCount() {
+//     let count = prompt("pick a number 1-5");
+//     for (i = 0; i < count; i++) {
+//         console.log(i);
+//     }
+// }
+
+// myCount();
+
+
+// function selectaleaf() {
+//     let selection = prompt ('how many apps would you like?');
+//     while (selection < 1 || selection >5 ) {
+//         selection = prompt ('must be between 1 and 5')
+// }
+// for (let i = 0; i < selection; i++) {
+//     console.log(i)
+//     document.write("<img src= 'images/stuff alt='leaf' />")
+// }
+
+
+
+<body>
+    <script>
+        alert("hey there, how can we help?");
+        console.log("choose wisely!");
+        let usersName = prompt("who is asking?");
+        console.log("it is I: " + usersName);
+        document.write("hello and come on in, " + usersName);
+
+        let response = prompt("do you like puerto rican food?")
+
+        if (response == "yes") {
+            alert("you are in the right place for sure!");
+            document.write("you are in the right place for sure!");
+        } else if (response == "no") {
+            alert("why would you come here then?");
+            document.write("why would you come here then?");
+        } else {
+            alert("why would you say that?");
+            document.write("why would you say that?");
+        }
+        
+    </script>
+
+
+</body>
+
+
 
 
 
