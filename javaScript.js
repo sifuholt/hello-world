d9afcfd69884bb4542e747a475d2b12dfe238894
Kenneth Holt <kingholt@gmail.com>
2022-11-17 16:45:30 -0800

    add notes

---
+++ javaScript.js
@@ -0,0 +1,57 @@
+
+/* 
+function rateMyPage(){
+    let rating = prompt("how many stars would you rate my page?");
+    for (let i = 0; i < rating; i++){
+        document.write(",img src='star.jpg' alt="shiny gold star" />
+    }
+} */
+
+function first(){
+alert('Halt!, who goes there??');
+console.log('this is my first attempt at this!');
+let usersName = prompt("Who is it that wishes to enter?");
+console.log("The user's name is: ", usersName);
+document.write("Hello and welcome to my first attempt at this, " + usersName);
+}
+first();
+
+
+function userName() {
+    let user=prompt ("what is your name??");
+
+    if (user === ""){
+        user=prompt ("but seriously give me");
+    }
+    else {
+        console.log(user);
+        document.write("let's begin adv" + user);
+    }
+
+    return user;
+
+}
+
+function exp() {
+    document.write("get ready for fun");
+
+if (response == "yes"){
+    alert("youve come here");
+}
+
+}
+
+
+function userName(){
+    let user=prompt ("what is name");
+
+    while (user=== "") {
+        user=prompt ("but serisolky");
+    }
+
+    console.log(user);
+    document.write("lets begin adcventure" + user);
+
+    return user;
+}
+
